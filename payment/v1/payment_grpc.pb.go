dd98597214d3d97ba323c145345a17643ef8d9db
usenbai-nur <usenbai.nur@users.noreply.github.com>
2026-04-20 10:52:00 +0000

    generate protobuf files

--- payment/v1/payment_grpc.pb.go
+++ payment/v1/payment_grpc.pb.go
@@ -21,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion9
 const (
 	PaymentService_ProcessPayment_FullMethodName      = "/payment.v1.PaymentService/ProcessPayment"
 	PaymentService_GetPaymentByOrderID_FullMethodName = "/payment.v1.PaymentService/GetPaymentByOrderID"
+	PaymentService_GetPaymentStats_FullMethodName     = "/payment.v1.PaymentService/GetPaymentStats"
 )
 
 // PaymentServiceClient is the client API for PaymentService service.
@@ -29,6 +30,7 @@ const (
 type PaymentServiceClient interface {
 	ProcessPayment(ctx context.Context, in *PaymentRequest, opts ...grpc.CallOption) (*PaymentResponse, error)
 	GetPaymentByOrderID(ctx context.Context, in *GetPaymentByOrderIDRequest, opts ...grpc.CallOption) (*PaymentResponse, error)
+	GetPaymentStats(ctx context.Context, in *GetPaymentStatsRequest, opts ...grpc.CallOption) (*PaymentStats, error)
 }
 
 type paymentServiceClient struct {
@@ -59,12 +61,23 @@ func (c *paymentServiceClient) GetPaymentByOrderID(ctx context.Context, in *GetP
 	return out, nil
 }
 
+func (c *paymentServiceClient) GetPaymentStats(ctx context.Context, in *GetPaymentStatsRequest, opts ...grpc.CallOption) (*PaymentStats, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(PaymentStats)
+	err := c.cc.Invoke(ctx, PaymentService_GetPaymentStats_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PaymentServiceServer is the server API for PaymentService service.
 // All implementations must embed UnimplementedPaymentServiceServer
 // for forward compatibility.
 type PaymentServiceServer interface {
 	ProcessPayment(context.Context, *PaymentRequest) (*PaymentResponse, error)
 	GetPaymentByOrderID(context.Context, *GetPaymentByOrderIDRequest) (*PaymentResponse, error)
+	GetPaymentStats(context.Context, *GetPaymentStatsRequest) (*PaymentStats, error)
 	mustEmbedUnimplementedPaymentServiceServer()
 }
 
@@ -80,6 +93,9 @@ func (UnimplementedPaymentServiceServer) ProcessPayment(context.Context, *Paymen
 }
 func (UnimplementedPaymentServiceServer) GetPaymentByOrderID(context.Context, *GetPaymentByOrderIDRequest) (*PaymentResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetPaymentByOrderID not implemented")
+}
+func (UnimplementedPaymentServiceServer) GetPaymentStats(context.Context, *GetPaymentStatsRequest) (*PaymentStats, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPaymentStats not implemented")
 }
 func (UnimplementedPaymentServiceServer) mustEmbedUnimplementedPaymentServiceServer() {}
 func (UnimplementedPaymentServiceServer) testEmbeddedByValue()                        {}
@@ -138,6 +154,24 @@ func _PaymentService_GetPaymentByOrderID_Handler(srv interface{}, ctx context.Co
 	return interceptor(ctx, in, info, handler)
 }
 
+func _PaymentService_GetPaymentStats_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetPaymentStatsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PaymentServiceServer).GetPaymentStats(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PaymentService_GetPaymentStats_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PaymentServiceServer).GetPaymentStats(ctx, req.(*GetPaymentStatsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // PaymentService_ServiceDesc is the grpc.ServiceDesc for PaymentService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -152,6 +186,10 @@ var PaymentService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetPaymentByOrderID",
 			Handler:    _PaymentService_GetPaymentByOrderID_Handler,
+		},
+		{
+			MethodName: "GetPaymentStats",
+			Handler:    _PaymentService_GetPaymentStats_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
